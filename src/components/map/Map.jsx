75c6e1f77a75161ac2ce34cfa688c0659796f7f7
Phoebe <phyuhala@gmail.com>
2022-03-13 14:12:22 -0700

    implmented google-map-react

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
+// api
+import GoogleMapReact from 'google-map-react'
+
+// MaterialUI components
+import Rating from '@material-ui/lab'
+
+// icons
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+
+import styled from 'styled-components';
+const Container = styled.div`
+   height:100vh ;
+`
 const Map = () => {
+  const coordinates = { lat: 0, lng: 0}
+
   return (
-    <div>Map</div>
+    <Container>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key:process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          // options={''}
+          // onChange={''}
+          // onChildClick={''}
+        >
+
+
+        </GoogleMapReact>
+    </Container>
   )
 }
 
